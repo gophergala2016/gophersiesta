5fac9a03a0c1d8c32a3f97974614b6484129c5ee
Daniel Esteban <conejo@conejo.me>
2016-01-23 19:25:55 +0100

    added ethereal as storage

---
+++ server/storage/ethereal.go
@@ -0,0 +1,61 @@
+package server
+
+
+type Ethereal struct {
+	options map[string]map[string]map[string]string
+}
+
+func (this *Ethereal) Init() {
+	this.options = make(map[string]map[string]map[string]string)
+}
+
+func (this *Ethereal) SetOption(appname, label, variable, value string) {
+
+	if label=="" {
+		label = "default"
+	}
+
+	if this.options[appname]==nil {
+		this.options[appname] = make(map[string]map[string]string)
+	}
+	if this.options[appname][label]==nil {
+		this.options[appname][label] = make(map[string]string)
+	}
+
+	this.options[appname][label][variable] = value
+
+}
+
+func (this *Ethereal) GetOption(appname, label, variable string) string {
+
+	if label=="" {
+		label = "default"
+	}
+
+	if this.options[appname]==nil {
+		this.options[appname] = make(map[string]map[string]string)
+	}
+	if this.options[appname][label]==nil {
+		this.options[appname][label] = make(map[string]string)
+	}
+
+	return this.options[appname][label][variable]
+
+}
+
+func (this *Ethereal) GetOptions(appname, label string) map[string]string {
+
+	if label=="" {
+		label = "default"
+	}
+
+	if this.options[appname]==nil {
+		this.options[appname] = make(map[string]map[string]string)
+	}
+	if this.options[appname][label]==nil {
+		this.options[appname][label] = make(map[string]string)
+	}
+
+	return this.options[appname][label]
+
+}
