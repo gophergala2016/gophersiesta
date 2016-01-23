20662bd6b41cef3ae7c3b8f4782a7e86bf7e9c34
morfeo8marc <morfeo8marc@gmail.com>
2016-01-23 14:37:18 +0100

    Add starting message

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
-import "github.com/gophergala2016/gophersiesta/server"
+import (
+	"github.com/gophergala2016/gophersiesta/server"
+	"fmt"
+)
 
 // Another entry point to start the server
 func main()  {
+	fmt.Println("Starting server")
 	server.StartServer()
 }
