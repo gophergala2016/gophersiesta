325e2d28b25c33f808ac5c8bf0a3c168bfa2ea85
Daniel Esteban <conejo@conejo.me>
2016-01-23 12:21:53 +0100

    fix typo on README, added newline character to responses

--- server/server.go
+++ server/server.go
@@ -11,7 +11,7 @@ func main() {
 	// This handler will match /conf/appname but will not match neither /conf/ or /conf
 	router.GET("/conf/:appname", func(c *gin.Context) {
 		name := c.Param("appname")
-		c.String(http.StatusOK, "Config file  %s", name)
+		c.String(http.StatusOK, "Config file  %s \n", name)
 	})
 
 	// However, this one will match /conf/app1/ and also /conf/app1/send
@@ -19,7 +19,7 @@ func main() {
 	router.GET("/conf/:appname/*action", func(c *gin.Context) {
 		name := c.Param("appname")
 		action := c.Param("action")
-		message := name + " is " + action
+		message := name + " is " + action + "\n"
 		c.String(http.StatusOK, message)
 	})
 
